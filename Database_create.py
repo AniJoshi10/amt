0cdbc8f10b0818d4c1fdc7de8429a9a3fb3131dd
Ani Joshi <44496451+AniJoshi10@users.noreply.github.com>
2020-09-22 20:13:25 +0530

    Update Database_create.py

--- Database_create.py
+++ Database_create.py
@@ -6,7 +6,7 @@ import mysql.connector
 import csv
 
 
-mydb=mysql.connector.connect(host="localhost",user="root",passwd="AdityaRP23",database="practice")
+mydb=mysql.connector.connect(host="",user="",passwd="",database="")
 
 mycursor=mydb.cursor()
 mycursor.execute("CREATE TABLE Attendance1(name varchar(255),roll_no INT(10),class varchar(255),stats varchar(255),date varchar(255))")
